1682c166e330f3257f6990ef97935142d8191c1c
Anurag Srivastava <anuragkipm751@gmail.com>
2026-02-19 22:56:58 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8001/api"
+  // baseURL: "http://127.0.0.1:8001/api"
+  baseURL: "https://hrms-lite-backend-hibe.onrender.com/api/"
 });
 
